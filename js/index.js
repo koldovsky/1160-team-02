b5ddf5b939cecc9f6e708d367a556ca885563f82
AnStacy <niaiko.ana.stasia@gmail.com>
2024-03-17 21:06:03 +0200

    Add js

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function init() {
+    import('./new-arrivals-partial');
+}
+
+const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
+let loadedPartialsCount = 0;
+
+document.body.addEventListener('htmx:afterOnLoad', () => {
+    loadedPartialsCount++;
+    if (loadedPartialsCount === totalPartials) init();
+});
