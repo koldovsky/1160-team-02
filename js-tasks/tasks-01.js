70e51b48125bd5fc8a908802843a290048e1231c
Akak <wwwowchik@hotmail.com>
2024-03-18 03:03:37 +0200

    tasks-01.js add

---
+++ js-tasks/tasks-01.js
@@ -0,0 +1,29 @@
+// Messi Goals
+// https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+
+// team code here 1
+
+// Make negative
+// https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+// team code here 2
+
+// Game Move
+// https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+
+// team code here 3
+
+// Personalized Message
+// https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+
+// team code here 4
+
+// Keep Hydrated
+// https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+
+// team code here 5
+
+// Opposites Attract
+// https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+
+// team code here 6
