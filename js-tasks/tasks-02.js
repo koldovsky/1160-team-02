89be9898e069932caca26b8d5cf76311da8fc85c
Akak <wwwowchik@hotmail.com>
2024-03-18 19:50:54 +0200

    added structure inside

---
+++ js-tasks/tasks-02.js
@@ -0,0 +1,68 @@
+// Simple tasks (1-4):     =^.^= =^.^= =^.^= =^.^= =^.^= =^.^= =^.^= =^.^= =^.^=
+
+// 1. Convert a string to an array
+// https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+
+// team code here
+
+// 2. DNA to RNA Conversion
+// https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+
+// team code here
+
+// 3. Find Maximum and Minimum Values of a List
+// https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+
+// team code here
+
+// 4. Smallest value of an array
+// https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
+
+
+// Advanced tasks (not necessary) (5-7):    =^.^= =^.^= =^.^= =^.^= =^.^= =^.^=
+
+// 5. A wolf in sheep's clothing
+// https://www.codewars.com/kata/a-wolf-in-sheeps-clothing/train/javascript
+
+// team code here
+
+// 6. Beginner - Lost Without a Map
+// https://www.codewars.com/kata/beginner-lost-without-a-map
+
+// team code here
+
+// 7. Find the first non-consecutive number
+// https://www.codewars.com/kata/find-the-first-non-consecutive-number/train/javascript
+
+
+// Bonus tasks (some extra) (8-13):    =^.^= =^.^= =^.^= =^.^= =^.^= =^.^= =^.^=
+
+// 8. Double Integer (You Can't Code Under Pressure #1)
+// https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
+
+// team code here
+
+// 9. Twice as old 
+// https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
+
+// team code here
+
+// 10. Return n-th even number (Get Nth Even Number)
+// https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/train/javascript
+
+// team code here
+
+// 11. What's the real floor
+// https://www.codewars.com/kata/574b3b1599d8f897470018f6/train/javascript
+
+// team code here
+
+// 12. Clock (Beginner Series #2 Clock)
+// https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/javascript
+
+// team code here
+
+// 13. Is n divisible by x and y
+// https://www.codewars.com/kata/5545f109004975ea66000086/train/javascript
+
+// team code here
