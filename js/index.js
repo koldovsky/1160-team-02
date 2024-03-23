c59d3c166f416a2acfd0f5ca8f270badeb3e87d6
Akak <wwwowchik@hotmail.com>
2024-03-23 18:12:04 +0200

    cleaning index, adding 2 js files

--- js/index.js
+++ js/index.js
@@ -1,22 +1,8 @@
 function init() {
-  // please, add element names in the end of file-names if u dont mind, thx
-
-  /*import('./common.header-nav.js'); /*Новікова*/
-  /*import('./index.hero.js'); /*Новікова*/
-  /*import('./index.food-and-stuff.js'); /*Марченко*/
-  /*import('./index.about-us.js'); /*Чемісов*/
-  /*import('./index.trending-products.js'); /*Мачіха*/
-  /*import ("./index.brands.js"); /*Кундукова*/
   import('./new-arrivals-partial'); /*Няйко*/
-  /*import('./index.new-arrivals.js'); /*Няйко*/
-  /*import('./index.promotion.js'); /*Леонович*/
-  /*import('./index.recommendations.js'); /*Дерев'янко*/
   import('./testimonials-carousel.js'); /*Наконечний*/
-  /*import('./index.dry-food.js'); /*Зоценко*/
-  /*import('./index.featured-product.js'); /*Никифорчин*/
-  /*import('./index.news-articles.js'); /*Калюжний*/
   /*import("./index.call-us.js"); /*Курилін*/
-  /*import('./common.footer.js'); /*Курилін*/
+  import('./common.footer.js'); /*Курилін*/
 }
 
 const totalPartials = document.querySelectorAll(
@@ -28,3 +14,45 @@ document.body.addEventListener('htmx:afterOnLoad', () => {
   loadedPartialsCount++;
   if (loadedPartialsCount === totalPartials) init();
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// please, add element names in the end of file-names if u dont mind, thx
+// and feel free to use the corresponding import string
+
+/*import('./common.header-nav.js'); /*Новікова*/
+/*import('./index.hero.js'); /*Новікова*/
+/*import('./index.food-and-stuff.js'); /*Марченко*/
+/*import('./index.about-us.js'); /*Чемісов*/
+/*import('./index.trending-products.js'); /*Мачіха*/
+/*import('./index.brands.js'); /*Кундукова*/
+
+/*import('./index.new-arrivals.js'); /*Няйко*/
+/*import('./index.promotion.js'); /*Леонович*/
+/*import('./index.recommendations.js'); /*Дерев'янко*/
+
+/*import('./index.dry-food.js'); /*Зоценко*/
+/*import('./index.featured-product.js'); /*Никифорчин*/
+/*import('./index.news-articles.js'); /*Калюжний*/
