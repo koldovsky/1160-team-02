6904b131c5526cf6b1964dbb6b3212c8bb870a7f
ANOVIKOVA21 <82145323+ANOVIKOVA21@users.noreply.github.com>
2024-03-24 17:22:59 +0000

    refactor code

--- js/burger-button.js
+++ js/burger-button.js
@@ -1,10 +1,8 @@
-htmx.onLoad(function () {
-  const burgerBtn = document.querySelector(".header__burger-btn");
-  const navElement = document.querySelector(".header__nav");
+const burgerBtn = document.querySelector(".header__burger-btn");
+const navElement = document.querySelector(".header__nav");
 
-  burgerBtn.addEventListener("click", () => {
-    burgerBtn.classList.toggle("burger-active");
-    navElement.classList.toggle("active-menu");
-    document.body.classList.toggle("lock");
-  });
+burgerBtn.addEventListener("click", () => {
+  burgerBtn.classList.toggle("burger-active");
+  navElement.classList.toggle("active-menu");
+  document.body.classList.toggle("lock");
 });
