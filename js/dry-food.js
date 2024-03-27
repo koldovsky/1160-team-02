52b2773e35f6410193dc4949cc8ca67101be072c
Diana_Zotsenko <90691365+DZotsenko@users.noreply.github.com>
2024-03-27 10:12:23 +0000

    added api

--- js/dry-food.js
+++ js/dry-food.js
@@ -1,81 +1,18 @@
 // Зоценко Діана__block 10
 
-const dogsProducts = [
-    {
-        "id" : "1",
-        "image" : "img/dry-food-images/natural-beef.png",
-        "title" : "Natural Beef",
-        "price" : "$2",
-        "ingredients" : "Meat And Bone Meal, Corn Gluten Meal, Animal Fat, Soybean Meal, Natural Flavor."
-    },
-    {
-        "id" : "2",
-        "image" : "img/dry-food-images/natural-chicken.png",
-        "title" : "Natural Chicken",
-        "price" : "$3",
-        "ingredients" : "Deboned Chicken, Chicken Meal, Brown Rice, Corn, Corn Gluten, Meal, Natural Flavor."
-    },
-    {
-        "id" : "3",
-        "image" : "img/dry-food-images/natural-pork.png",
-        "title" : "Natural Pork",
-        "price" : "$2",
-        "ingredients" : "Deboned Lamb, Chicken Meal, Split Peas, Lentils, Chicken Fat, Pork Meal, Dried Chicken, Pea."
-    },
-];
+const responseDogs = await fetch("api/dry-food/dogs-products.json");
+const dogsProducts = await responseDogs.json();
 
-const catsProducts = [
-    {
-        "id" : "4",
-        "image" : "img/dry-food-images/turkey-pate.png",
-        "title" : "Turkey Pate",
-        "price" : "$1",
-        "ingredients" : "Deboned Turkey, Turkey Broth, Turkey Liver, Dried Egg Product, Cranberries, Calcium Carbonate."
-    },
-    {
-        "id" : "5",
-        "image" : "img/dry-food-images/tuna-salmon.png",
-        "title" : "Tuna & Salmon",
-        "price" : "$2",
-        "ingredients" : "Chicken, Poultry Broth, Liver, Meat By-Products, Salmon, Tuna, Guar Gum, Minerals."
-    },
-    {
-        "id" : "6",
-        "image" : "img/dry-food-images/chicken-slice.png",
-        "title" : "Chicken Slice",
-        "price" : "$1",
-        "ingredients" : "Wheat Gluten, Liver, Chicken, Meat By-Products, Tomatoes, Carrots, Shrimp, Soy Flour, Salt."
-    },
-];
+const responseCats = await fetch("api/dry-food/cats-products.json");
+const catsProducts = await responseCats.json();
 
-const otherProducts = [
-    {
-        "id" : "7",
-        "image" : "img/dry-food-images/grain-nuts.png",
-        "title" : "Grain & Nuts",
-        "price" : "$3",
-        "ingredients" : "Whole Brown Rice, Oat Groats, Wheat Bran, Wheat, Soybean Meal, Soybean Hulls."
-    },
-    {
-        "id" : "8",
-        "image" : "img/dry-food-images/fish-food.png",
-        "title" : "Fish Food",
-        "price" : "$5",
-        "ingredients" : "Dried Yeast, Ground Brown Rice, Shrimp Meal, Dried Fish Protein Digest, Wheat Gluten."
-    },
-    {
-        "id" : "9",
-        "image" : "img/dry-food-images/parrots-food.png",
-        "title" : "Parrots Food",
-        "price" : "$7",
-        "ingredients" : "Canary Grass Seed, White Millet, Oat Groats, Ground Corn, Red Millet, Flax Seed."
-    },
-];
+const responseOther = await fetch("api/dry-food/other-products.json");
+const otherProducts = await responseOther.json();
 
 function renderDogsProducts(dogsProducts) {
-    let productsDomString = '';
-    for (const product of dogsProducts) {
-        productsDomString += `
+  let productsDomString = "";
+  for (const product of dogsProducts) {
+    productsDomString += `
         <article>
             <img class="dry-food__product-icon" src="${product.image}" alt="${product.title}" width="120">
             <div class="description">
@@ -87,15 +24,15 @@ function renderDogsProducts(dogsProducts) {
                 <span class="ingredients">${product.ingredients}</span>
             </div>
         </article>`;
-    }
-    const productsContainer = document.querySelector('.dogs-products-list');
-    productsContainer.innerHTML = productsDomString;
+  }
+  const productsContainer = document.querySelector(".dogs-products-list");
+  productsContainer.innerHTML = productsDomString;
 }
 
 function renderCatsProducts(catsProducts) {
-    let productsDomString = '';
-    for (const product of catsProducts) {
-        productsDomString += `
+  let productsDomString = "";
+  for (const product of catsProducts) {
+    productsDomString += `
         <article>
             <img class="dry-food__product-icon" src="${product.image}" alt="${product.title}" width="120">
             <div class="description">
@@ -107,15 +44,15 @@ function renderCatsProducts(catsProducts) {
                 <span class="ingredients">${product.ingredients}</span>
             </div>
         </article>`;
-    }
-    const productsContainer = document.querySelector('.cats-products-list');
-    productsContainer.innerHTML = productsDomString;
+  }
+  const productsContainer = document.querySelector(".cats-products-list");
+  productsContainer.innerHTML = productsDomString;
 }
 
 function renderOtherProducts(otherProducts) {
-    let productsDomString = '';
-    for (const product of otherProducts) {
-        productsDomString += `
+  let productsDomString = "";
+  for (const product of otherProducts) {
+    productsDomString += `
         <article>
             <img class="dry-food__product-icon" src="${product.image}" alt="${product.title}" width="120">
             <div class="description">
@@ -127,9 +64,9 @@ function renderOtherProducts(otherProducts) {
                 <span class="ingredients">${product.ingredients}</span>
             </div>
         </article>`;
-    }
-    const productsContainer = document.querySelector('.other-products-list');
-    productsContainer.innerHTML = productsDomString;
+  }
+  const productsContainer = document.querySelector(".other-products-list");
+  productsContainer.innerHTML = productsDomString;
 }
 
 renderDogsProducts(dogsProducts);
