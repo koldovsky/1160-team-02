acb3acde54e1da58672de3898324cb2af3f19616
Akak <wwwowchik@hotmail.com>
2024-04-02 09:25:21 +0300

    fixing errors whom to blame?

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 function init() {
-  import("./burger-button.js");
-  import("./new-arrivals.carousel.js"); /*Няйко*/
+  import('./burger-button.js');
+  import('./new-arrivals.carousel.js'); /*Няйко*/
   import('./index.promotion-timer.js'); /*Леонович*/
-  import("./testimonials-carousel.js"); /*Наконечний*/
-  /*import("./index.call-us.js"); /*Курилін*/
-  import("./common.footer.js"); /*Курилін*/
-  import("./dry-food.js"); /*Зоценко*/
+  import('./testimonials-carousel.js'); /*Наконечний*/
+  import('./index.call-us.sale-week.countdown.js'); /*Курилін*/
+  import('./common.footer.copyright.clock.js'); /*Курилін*/
+  import('./dry-food.js'); /*Зоценко*/
 }
 
 const totalPartials = document.querySelectorAll(
@@ -13,25 +13,7 @@ const totalPartials = document.querySelectorAll(
 ).length;
 let loadedPartialsCount = 0;
 
-document.body.addEventListener("htmx:afterOnLoad", () => {
+document.body.addEventListener('htmx:afterOnLoad', () => {
   loadedPartialsCount++;
   if (loadedPartialsCount === totalPartials) init();
 });
-
-// please, add element names in the end of file-names if u dont mind, thx
-// and feel free to use the corresponding import string
-
-/*import('./common.header-nav.js'); /*Новікова*/
-/*import('./index.hero.js'); /*Новікова*/
-/*import('./index.food-and-stuff.js'); /*Марченко*/
-/*import('./index.about-us.js'); /*Чемісов*/
-/*import('./index.trending-products.js'); /*Мачіха*/
-/*import('./index.brands.js'); /*Кундукова*/
-
-/*import('./index.new-arrivals.js'); /*Няйко*/
-
-/*import('./index.recommendations.js'); /*Дерев'янко*/
-
-/*import('./index.dry-food.js'); /*Зоценко*/
-/*import('./index.featured-product.js'); /*Никифорчин*/
-/*import('./index.news-articles.js'); /*Калюжний*/
