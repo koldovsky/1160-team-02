c32ad30aaf87d8d426c691788e6ca0ef680c919b
Olexsandrik <119703838+Olexsandrik@users.noreply.github.com>
2024-04-04 08:50:57 +0000

    refresh featured container

--- js/featured-product.js
+++ js/featured-product.js
@@ -13,7 +13,7 @@ function renderCode(productList, rate=1){
         productDomString+=`
         <div class="slider__main-slider">
         
-        <p class="slider__title">Store homepage</p>
+        
         <div class="slide">
           <img
             class="slide-item"
@@ -41,7 +41,7 @@ function renderCode(productList, rate=1){
 
             <p class="product__code">${product.productCode}</p>
 
-            <p class="buyspace__price">$${(product.price*rate).toFixed(2)}</p>
+            <p class="buyspace__price">Price:${(product.price*rate).toFixed(2)}</p>
           </div>
 
           <div class="buyspace__line"></div>
